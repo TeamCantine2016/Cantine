5bca73ad67e1176556e48439091b5af2d6e1a8e7
Florian <Florian@FLORIAN-PC>
2016-05-19 17:12:51 +0200

    Fin de la form_VisualisationFamille

--- ProjetCantine/Vues/Form_VisualisationFamille.Designer.cs
+++ ProjetCantine/Vues/Form_VisualisationFamille.Designer.cs
@@ -37,13 +37,7 @@
             this.tAListeFamillesBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.db_cantineDataSet = new ProjetCantine.Models.db_cantineDataSet();
             this.groupBox_Récap = new System.Windows.Forms.GroupBox();
-            this.button_Annuler = new System.Windows.Forms.Button();
-            this.button_Valider = new System.Windows.Forms.Button();
             this.dataGridView_Membre = new System.Windows.Forms.DataGridView();
-            this.label_EnvoyerFacture = new System.Windows.Forms.Label();
-            this.label_GénérerFacture = new System.Windows.Forms.Label();
-            this.comboBox_Envoi = new System.Windows.Forms.ComboBox();
-            this.comboBox_Génération = new System.Windows.Forms.ComboBox();
             this.textBox_Téléphone = new System.Windows.Forms.TextBox();
             this.textBox_Email = new System.Windows.Forms.TextBox();
             this.textBox_Adresse = new System.Windows.Forms.TextBox();
@@ -53,6 +47,10 @@
             this.label_Adresse = new System.Windows.Forms.Label();
             this.label_Nom = new System.Windows.Forms.Label();
             this.tA_Liste_FamillesTableAdapter = new ProjetCantine.Models.db_cantineDataSetTableAdapters.TA_Liste_FamillesTableAdapter();
+            this.button_NomReset = new System.Windows.Forms.Button();
+            this.button_TelReset = new System.Windows.Forms.Button();
+            this.textBox_nbEnfant = new System.Windows.Forms.TextBox();
+            this.label_nbEnfants = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Famille)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.tAListeFamillesBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.db_cantineDataSet)).BeginInit();
@@ -62,7 +60,7 @@
             // 
             // textBox_NomRech
             // 
-            this.textBox_NomRech.Location = new System.Drawing.Point(183, 27);
+            this.textBox_NomRech.Location = new System.Drawing.Point(189, 37);
             this.textBox_NomRech.Name = "textBox_NomRech";
             this.textBox_NomRech.Size = new System.Drawing.Size(282, 20);
             this.textBox_NomRech.TabIndex = 0;
@@ -70,7 +68,7 @@
             // 
             // textBox_TéléphoneRech
             // 
-            this.textBox_TéléphoneRech.Location = new System.Drawing.Point(183, 53);
+            this.textBox_TéléphoneRech.Location = new System.Drawing.Point(189, 63);
             this.textBox_TéléphoneRech.Name = "textBox_TéléphoneRech";
             this.textBox_TéléphoneRech.Size = new System.Drawing.Size(282, 20);
             this.textBox_TéléphoneRech.TabIndex = 5;
@@ -79,7 +77,7 @@
             // label_NomRech
             // 
             this.label_NomRech.AutoSize = true;
-            this.label_NomRech.Location = new System.Drawing.Point(63, 30);
+            this.label_NomRech.Location = new System.Drawing.Point(69, 40);
             this.label_NomRech.Name = "label_NomRech";
             this.label_NomRech.Size = new System.Drawing.Size(85, 13);
             this.label_NomRech.TabIndex = 2;
@@ -88,7 +86,7 @@
             // label_TéléphoneRech
             // 
             this.label_TéléphoneRech.AutoSize = true;
-            this.label_TéléphoneRech.Location = new System.Drawing.Point(63, 56);
+            this.label_TéléphoneRech.Location = new System.Drawing.Point(69, 66);
             this.label_TéléphoneRech.Name = "label_TéléphoneRech";
             this.label_TéléphoneRech.Size = new System.Drawing.Size(114, 13);
             this.label_TéléphoneRech.TabIndex = 3;
@@ -100,11 +98,11 @@
             this.dataGridView_Famille.AllowUserToDeleteRows = false;
             this.dataGridView_Famille.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView_Famille.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView_Famille.Location = new System.Drawing.Point(72, 90);
+            this.dataGridView_Famille.Location = new System.Drawing.Point(72, 107);
             this.dataGridView_Famille.Name = "dataGridView_Famille";
             this.dataGridView_Famille.ReadOnly = true;
             this.dataGridView_Famille.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridView_Famille.Size = new System.Drawing.Size(938, 150);
+            this.dataGridView_Famille.Size = new System.Drawing.Size(938, 185);
             this.dataGridView_Famille.TabIndex = 4;
             this.dataGridView_Famille.TabStop = false;
             this.dataGridView_Famille.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView_Famille_CellEnter);
@@ -121,13 +119,9 @@
             // 
             // groupBox_Récap
             // 
-            this.groupBox_Récap.Controls.Add(this.button_Annuler);
-            this.groupBox_Récap.Controls.Add(this.button_Valider);
+            this.groupBox_Récap.Controls.Add(this.textBox_nbEnfant);
+            this.groupBox_Récap.Controls.Add(this.label_nbEnfants);
             this.groupBox_Récap.Controls.Add(this.dataGridView_Membre);
-            this.groupBox_Récap.Controls.Add(this.label_EnvoyerFacture);
-            this.groupBox_Récap.Controls.Add(this.label_GénérerFacture);
-            this.groupBox_Récap.Controls.Add(this.comboBox_Envoi);
-            this.groupBox_Récap.Controls.Add(this.comboBox_Génération);
             this.groupBox_Récap.Controls.Add(this.textBox_Téléphone);
             this.groupBox_Récap.Controls.Add(this.textBox_Email);
             this.groupBox_Récap.Controls.Add(this.textBox_Adresse);
@@ -136,31 +130,12 @@
             this.groupBox_Récap.Controls.Add(this.label_Email);
             this.groupBox_Récap.Controls.Add(this.label_Adresse);
             this.groupBox_Récap.Controls.Add(this.label_Nom);
-            this.groupBox_Récap.Location = new System.Drawing.Point(66, 275);
+            this.groupBox_Récap.Location = new System.Drawing.Point(66, 326);
             this.groupBox_Récap.Name = "groupBox_Récap";
-            this.groupBox_Récap.Size = new System.Drawing.Size(950, 355);
+            this.groupBox_Récap.Size = new System.Drawing.Size(950, 304);
             this.groupBox_Récap.TabIndex = 8;
             this.groupBox_Récap.TabStop = false;
             this.groupBox_Récap.Text = "Information Famille";
-            // 
-            // button_Annuler
-            // 
-            this.button_Annuler.Location = new System.Drawing.Point(592, 305);
-            this.button_Annuler.Name = "button_Annuler";
-            this.button_Annuler.Size = new System.Drawing.Size(200, 35);
-            this.button_Annuler.TabIndex = 25;
-            this.button_Annuler.Text = "Annuler";
-            this.button_Annuler.UseVisualStyleBackColor = true;
-            this.button_Annuler.Click += new System.EventHandler(this.button_Annuler_Click);
-            // 
-            // button_Valider
-            // 
-            this.button_Valider.Location = new System.Drawing.Point(159, 305);
-            this.button_Valider.Name = "button_Valider";
-            this.button_Valider.Size = new System.Drawing.Size(200, 35);
-            this.button_Valider.TabIndex = 20;
-            this.button_Valider.Text = "Valider";
-            this.button_Valider.UseVisualStyleBackColor = true;
             // 
             // dataGridView_Membre
             // 
@@ -171,50 +146,10 @@
             this.dataGridView_Membre.Location = new System.Drawing.Point(6, 140);
             this.dataGridView_Membre.Name = "dataGridView_Membre";
             this.dataGridView_Membre.ReadOnly = true;
+            this.dataGridView_Membre.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dataGridView_Membre.Size = new System.Drawing.Size(938, 150);
             this.dataGridView_Membre.TabIndex = 19;
             this.dataGridView_Membre.TabStop = false;
-            // 
-            // label_EnvoyerFacture
-            // 
-            this.label_EnvoyerFacture.AutoSize = true;
-            this.label_EnvoyerFacture.Location = new System.Drawing.Point(706, 32);
-            this.label_EnvoyerFacture.Name = "label_EnvoyerFacture";
-            this.label_EnvoyerFacture.Size = new System.Drawing.Size(88, 13);
-            this.label_EnvoyerFacture.TabIndex = 18;
-            this.label_EnvoyerFacture.Text = "Envoyer facture :";
-            // 
-            // label_GénérerFacture
-            // 
-            this.label_GénérerFacture.AutoSize = true;
-            this.label_GénérerFacture.Location = new System.Drawing.Point(472, 32);
-            this.label_GénérerFacture.Name = "label_GénérerFacture";
-            this.label_GénérerFacture.Size = new System.Drawing.Size(111, 13);
-            this.label_GénérerFacture.TabIndex = 18;
-            this.label_GénérerFacture.Text = "Générer facture pour :";
-            // 
-            // comboBox_Envoi
-            // 
-            this.comboBox_Envoi.FormattingEnabled = true;
-            this.comboBox_Envoi.Items.AddRange(new object[] {
-            "Courrier",
-            "Email",
-            "Les 2"});
-            this.comboBox_Envoi.Location = new System.Drawing.Point(709, 55);
-            this.comboBox_Envoi.Name = "comboBox_Envoi";
-            this.comboBox_Envoi.Size = new System.Drawing.Size(200, 21);
-            this.comboBox_Envoi.TabIndex = 15;
-            // 
-            // comboBox_Génération
-            // 
-            this.comboBox_Génération.FormattingEnabled = true;
-            this.comboBox_Génération.Items.AddRange(new object[] {
-            "Mois",
-            "Semaine"});
-            this.comboBox_Génération.Location = new System.Drawing.Point(475, 55);
-            this.comboBox_Génération.Name = "comboBox_Génération";
-            this.comboBox_Génération.Size = new System.Drawing.Size(200, 21);
-            this.comboBox_Génération.TabIndex = 10;
             // 
             // textBox_Téléphone
             // 
@@ -280,13 +215,50 @@
             this.label_Nom.AutoSize = true;
             this.label_Nom.Location = new System.Drawing.Point(40, 32);
             this.label_Nom.Name = "label_Nom";
-            this.label_Nom.Size = new System.Drawing.Size(29, 13);
+            this.label_Nom.Size = new System.Drawing.Size(39, 13);
             this.label_Nom.TabIndex = 11;
-            this.label_Nom.Text = "Nom";
+            this.label_Nom.Text = "Famille";
             // 
             // tA_Liste_FamillesTableAdapter
             // 
             this.tA_Liste_FamillesTableAdapter.ClearBeforeFill = true;
+            // 
+            // button_NomReset
+            // 
+            this.button_NomReset.Location = new System.Drawing.Point(477, 35);
+            this.button_NomReset.Name = "button_NomReset";
+            this.button_NomReset.Size = new System.Drawing.Size(50, 23);
+            this.button_NomReset.TabIndex = 9;
+            this.button_NomReset.Text = "Reset";
+            this.button_NomReset.UseVisualStyleBackColor = true;
+            this.button_NomReset.Click += new System.EventHandler(this.button_NomReset_Click);
+            // 
+            // button_TelReset
+            // 
+            this.button_TelReset.Location = new System.Drawing.Point(477, 61);
+            this.button_TelReset.Name = "button_TelReset";
+            this.button_TelReset.Size = new System.Drawing.Size(50, 23);
+            this.button_TelReset.TabIndex = 10;
+            this.button_TelReset.Text = "Reset";
+            this.button_TelReset.UseVisualStyleBackColor = true;
+            this.button_TelReset.Click += new System.EventHandler(this.button_TelReset_Click);
+            // 
+            // textBox_nbEnfant
+            // 
+            this.textBox_nbEnfant.Enabled = false;
+            this.textBox_nbEnfant.Location = new System.Drawing.Point(591, 29);
+            this.textBox_nbEnfant.Name = "textBox_nbEnfant";
+            this.textBox_nbEnfant.Size = new System.Drawing.Size(62, 20);
+            this.textBox_nbEnfant.TabIndex = 21;
+            // 
+            // label_nbEnfants
+            // 
+            this.label_nbEnfants.AutoSize = true;
+            this.label_nbEnfants.Location = new System.Drawing.Point(455, 32);
+            this.label_nbEnfants.Name = "label_nbEnfants";
+            this.label_nbEnfants.Size = new System.Drawing.Size(130, 13);
+            this.label_nbEnfants.TabIndex = 20;
+            this.label_nbEnfants.Text = "Nombre d\'enfant à charge";
             // 
             // Form_VisualisationFamille
             // 
@@ -294,6 +266,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1084, 662);
             this.ControlBox = false;
+            this.Controls.Add(this.button_TelReset);
+            this.Controls.Add(this.button_NomReset);
             this.Controls.Add(this.groupBox_Récap);
             this.Controls.Add(this.dataGridView_Famille);
             this.Controls.Add(this.label_TéléphoneRech);
@@ -322,8 +296,6 @@
         private System.Windows.Forms.Label label_TéléphoneRech;
         private System.Windows.Forms.DataGridView dataGridView_Famille;
         private System.Windows.Forms.GroupBox groupBox_Récap;
-        private System.Windows.Forms.ComboBox comboBox_Envoi;
-        private System.Windows.Forms.ComboBox comboBox_Génération;
         private System.Windows.Forms.TextBox textBox_Téléphone;
         private System.Windows.Forms.TextBox textBox_Email;
         private System.Windows.Forms.TextBox textBox_Adresse;
@@ -332,13 +304,13 @@
         private System.Windows.Forms.Label label_Email;
         private System.Windows.Forms.Label label_Adresse;
         private System.Windows.Forms.Label label_Nom;
-        private System.Windows.Forms.Label label_EnvoyerFacture;
-        private System.Windows.Forms.Label label_GénérerFacture;
-        private System.Windows.Forms.Button button_Annuler;
-        private System.Windows.Forms.Button button_Valider;
         private System.Windows.Forms.DataGridView dataGridView_Membre;
         private Models.db_cantineDataSet db_cantineDataSet;
         private System.Windows.Forms.BindingSource tAListeFamillesBindingSource;
         private Models.db_cantineDataSetTableAdapters.TA_Liste_FamillesTableAdapter tA_Liste_FamillesTableAdapter;
+        private System.Windows.Forms.Button button_NomReset;
+        private System.Windows.Forms.Button button_TelReset;
+        private System.Windows.Forms.TextBox textBox_nbEnfant;
+        private System.Windows.Forms.Label label_nbEnfants;
     }
 }
